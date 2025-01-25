259a36148e0e02bbb4966c72f2ee75a9e91e538b
WllBrns <burnsw72@gmail.com>
2025-01-25 18:11:46 +1100

    Fix(Navigation): Home button navigates to home page now

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
