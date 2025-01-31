44850fb06cf81cc17b794b4971c5850562ad7737
WllBrns <burnsw72@gmail.com>
2025-01-31 17:38:45 +1100

    Fix(Navbar): Changed the navbar links to NavLinks to accommodate active styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,18 +7,10 @@ import { GoSearch } from "react-icons/go";
 import { IoPersonOutline } from "react-icons/io5";
 import { FiShoppingBag } from "react-icons/fi";
 
-const navbarLinks = [
-  { href: "/collections/men", text: "Men" },
-  { href: "/collections/women", text: "Women" },
-  { href: "/collections/unisex", text: "Unisex" },
-  { href: "/collections", text: "Collections" },
-  { href: "/blogs/news", text: "News" },
-];
-
 export const Header = () => {
   return (
     <StyledHeader>
-      <Navbar links={navbarLinks} />
+      <Navbar />
 
       <h1>
         <Button href="/Home" icon={<img src={Logo} alt="Mock.Shop" />} />
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,20 @@
-import { StyledButton } from "../button/Button.styled";
 import { StyledNavbar } from "./Navbar.styled.js";
-import { Link } from "react-router";
 
-export const Navbar = ({ links }) => {
-  const navbarLinks = links.map((link) => (
+import { StyledButton } from "../button/Button.styled";
+import { NavLink } from "react-router";
+
+const navbarLinks = [
+  { href: "collections/men", text: "Men" },
+  { href: "collections/women", text: "Women" },
+  { href: "collections/unisex", text: "Unisex" },
+  { href: "collections", text: "Collections" },
+  { href: "blogs/news", text: "News" },
+];
+
+export const Navbar = () => {
+  const links = navbarLinks.map((link) => (
     <li key={link.href}>
-      <StyledButton as={Link} to={link.href}>
+      <StyledButton as={NavLink} to={link.href}>
         {link.text}
       </StyledButton>
     </li>
@@ -13,7 +22,7 @@ export const Navbar = ({ links }) => {
 
   return (
     <StyledNavbar>
-      <ul>{navbarLinks}</ul>
+      <ul>{links}</ul>
     </StyledNavbar>
   );
 };
