f0a70d8ce1a5eeabc43fc6db2e467f2e876059bb
WllBrns <burnsw72@gmail.com>
2025-02-08 15:53:31 +1100

    Feature(Button): buttons can now work as links as necessary

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { StyledButton } from "./Button.styled.js";
 
-export const Button = ({ onClick, icon, as }) => {
+export const Button = ({ onClick, icon, as, href }) => {
   return (
-    <StyledButton as={as} onClick={onClick}>
+    <StyledButton as={as} onClick={onClick} to={href}>
       {icon}
     </StyledButton>
   );
