7cec0f74ce09012849db2767ee3a2efab472d21f
WllBrns <burnsw72@gmail.com>
2025-02-21 15:41:07 +1100

    Fix(link): removed unused import

--- src/components/link/Link.jsx
+++ src/components/link/Link.jsx
@@ -1,5 +1,4 @@
 import { StyledLink } from "./Link.styled.js";
-import { Link as RouterLink } from "react-router";
 
 export const Link = ({ href, icon }) => {
   return <StyledLink to={href}>{icon}</StyledLink>;
