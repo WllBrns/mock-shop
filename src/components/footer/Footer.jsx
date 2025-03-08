e87dca25c0ada099522822dc7034aa3b58e4f767
WllBrns <burnsw72@gmail.com>
2025-03-09 03:36:58 +1100

    Styling(footer): added container to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,11 @@ import { StyledFooter } from "./Footer.styled";
 export const Footer = () => {
   return (
     <StyledFooter>
-      <h2>Stay in the know</h2>
-      <input type="email" placeholder="Email" />
-      <p>Follow us on social media</p>
+      <div className="container">
+        <h2>Stay in the know</h2>
+        <input type="email" placeholder="Email" />
+        <p>Follow us on social media</p>
+      </div>
     </StyledFooter>
   );
 };
