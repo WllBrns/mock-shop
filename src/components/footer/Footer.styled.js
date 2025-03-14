65505b4f8cba7b11e59f00433bb23dd27a136135
WllBrns <burnsw72@gmail.com>
2025-03-14 15:39:55 +1100

    Styling(footer): Centered footer content and changed icon colour to white

--- src/components/footer/Footer.styled.js
+++ src/components/footer/Footer.styled.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
   background-color: black;
-
+  display: flex;
+  height: 26.625rem;
   h2, p{
     color: white;
   }
@@ -12,5 +13,10 @@ export const StyledFooter = styled.footer`
     border: solid;
     color: white;
     width: 100%;
+  }
+
+  .icons {
+
+    color: white;
   }
 `;
