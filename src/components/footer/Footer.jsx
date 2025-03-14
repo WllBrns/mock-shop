79d4d51ea96bca35be55b79f9dae6cdfe60ca540
WllBrns <burnsw72@gmail.com>
2025-03-14 15:39:22 +1100

    Styling(footer): added socials container to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,13 @@
 import { StyledFooter } from "./Footer.styled";
 
+import { FaTwitter } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaPinterest } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+import { FaTumblr } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+
 export const Footer = () => {
   return (
     <StyledFooter>
@@ -7,6 +15,15 @@ export const Footer = () => {
         <h2>Stay in the know</h2>
         <input type="email" placeholder="Email" />
         <p>Follow us on social media</p>
+        <p className="icons">
+          <FaTwitter />
+          <FaFacebookSquare />
+          <FaPinterest />
+          <FaInstagram />
+          <FaTiktok />
+          <FaTumblr />
+          <FaYoutube />
+        </p>
       </div>
     </StyledFooter>
   );
