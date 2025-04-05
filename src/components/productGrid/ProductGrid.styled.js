8f1fefcce4f091d9382fa9dfc1b0a611a5bde879
WllBrns <burnsw72@gmail.com>
2025-04-05 14:47:33 +1100

    Fix(styling): removed redundant styling

--- src/components/productGrid/ProductGrid.styled.js
+++ src/components/productGrid/ProductGrid.styled.js
@@ -4,7 +4,6 @@ export const StyledProductGrid = styled.div`
     display: grid;
     grid-template-columns: auto auto auto;
     gap: 2.5rem;
-    overflow: auto;
 
     @media (max-width: 1100px) {
       grid-template-columns: auto auto;
