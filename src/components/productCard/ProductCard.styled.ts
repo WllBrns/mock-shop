2a60b377e55551fe926ea123f3003218cc0e45c2
WllBrns <burnsw72@gmail.com>
2025-06-07 15:33:51 +1000

    Styling(productCard): added spacing to currency code and price on product cards

--- src/components/productCard/ProductCard.styled.ts
+++ src/components/productCard/ProductCard.styled.ts
@@ -1,46 +1,45 @@
 import styled from "styled-components";
 
 export const StyledProductCard = styled.div`
-  
-
-  padding: .75rem .75rem 2rem .75rem;
+  padding: 0.75rem 0.75rem 2rem 0.75rem;
   position: relative;
-  
+
   display: flex;
   flex-direction: column;
 
-
   img {
-    transition: transform .4s ease-in-out;
+    transition: transform 0.4s ease-in-out;
   }
 
-  &:hover{
-    img{ 
+  &:hover {
+    img {
       transform: scale(1.03);
     }
-}
+  }
 
   img {
     height: auto;
-    width:100%;
+    width: 100%;
     object-fit: contain;
   }
 
   .text {
-    padding: .625rem 1rem 0rem 1rem;
+    padding: 0rem 1rem 0rem 1rem;
+    display: flex;
+    gap: 0.3125rem;
   }
 
   .text * {
     margin: 0px;
   }
 
-  .text h3 {
+  h3 {
     margin-bottom: 10px;
-
+    padding: 0.625rem 1rem 0rem 1rem;
   }
-  
+
   .text h3::before {
-    content: '';
+    content: "";
     position: absolute;
     width: 100%;
     height: 100%;
@@ -53,7 +52,7 @@ export const StyledProductCard = styled.div`
     text-decoration: underline;
   }
 
-  .text p{
+  .text p {
     font-weight: 500;
   }
 `;
